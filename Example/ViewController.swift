5087b9e4c14855b1f4df7614bd19e3fe9bda387c
Takeshi Akutsu <takeshi.akutsu0303+github@gmail.com>
2020-04-25 19:51:40 +0900

    initialize Example app

---
+++ Example/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by 阿久津　岳志 on 2020/04/25.
+//  Copyright © 2020 阿久津　岳志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
