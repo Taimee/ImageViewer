253254d9a32ebb231b181164e8426935b25d3e95
Takeshi Akutsu <takeshi.akutsu0303+github@gmail.com>
2021-05-26 18:59:24 +0900

    add Source path

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ImageViewer",
+            path: "Sources",
             dependencies: []),
         .testTarget(
             name: "ImageViewerTests",
