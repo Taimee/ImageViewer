1e41e80efa436b06fcab9e7181c4ca4ca7273e70
Takeshi Akutsu <takeshi.akutsu0303+github@gmail.com>
2021-06-11 01:04:48 +0900

    change deployment target to 13.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ImageViewer",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "ImageViewer",
