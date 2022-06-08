0ccc0488a225a3ba31dd09a172777c93bffe860b
Takeshi Akutsu <takeshi.akutsu0303+github@gmail.com>
2022-06-08 23:14:10 +0900

    remove unnecessary files

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ImageViewer",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["../../README.md"]),
         .testTarget(
             name: "ImageViewerTests",
             dependencies: ["ImageViewer"]),
